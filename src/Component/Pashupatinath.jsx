1231b7c45dd3d4280db33dfef1eae17650d0644e
HRITIK18711 <hritikupadhyay2002@gmail.com>
2026-09-06 21:36:08 +0530

    Added new change in code

--- src/Component/Pashupatinath.jsx
+++ src/Component/Pashupatinath.jsx
@@ -7,7 +7,7 @@ const SectionImage = ({ src, source, alt = "Pashupatinath Temple Kathmandu Nepal
     </div>
 );
 
-const pashupatinath = () => {
+const Pashupatinath = () => {
     return (
         <div className="bg-gray-50 py-10 px-4">
             <div className="max-w-4xl mx-auto bg-white p-6 md:p-10 shadow rounded-lg mt-12">
@@ -346,4 +346,4 @@ const pashupatinath = () => {
     );
 };
 
-export default pashupatinath;
+export default Pashupatinath;
